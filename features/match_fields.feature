66ccb91bc7fbcdf2c73e27b943a75f80c17ea318
Ryan Burrows <rhburrows@crystalcommerce.com>
2010-06-30 08:44:49 -0700

    Add Feature test for matching on multiple fields

--- features/match_fields.feature
+++ features/match_fields.feature
@@ -16,5 +16,22 @@ Feature: jse matches specific fields within the json
     """
 
   Scenario: Exact match on multiple fields
+    Given I have a log file containing:
+    """
+    {"level":"INFO","message":"line one"}
+    {"level":"DEBUG","message":"line two"}
+    {"level":"ERROR","message":"line three"}
+    {"level":"INFO","message":"line four"}
+    """
+    When I run "jse level:INFO message:'line one'" on my log file
+    Then I should see:
+    """
+    {"level":"INFO","message":"line one"}
+    """
+    But I should not see:
+    """
+    {"level":"INFO","message":"line four"}
+    """
+
   Scenario: Regexp match on one field
   Scenario: Regexp match on multiple fields
--- features/step_definitions/match_fields_steps.rb
+++ features/step_definitions/match_fields_steps.rb
@@ -7,12 +7,19 @@ Given /^I have a log file containing:$/ do |string|
 end
 
 When /^I run "jse([^\"]*)" on my log file$/ do |opts|
-  JSE::CLI.execute(output, opts.split(' ') + [LOG_FILE_NAME])
+  options = parse_options(opts)
+  JSE::CLI.execute(output, options + [LOG_FILE_NAME])
 end
 
 Then /^I should see:$/ do |text|
   text.split("\n").each do |line|
     output.should include(line)
+  end
+end
+
+Then /^I should not see:$/ do |text|
+  text.split("\n").each do |line|
+    output.should_not include(line)
   end
 end
 
@@ -36,4 +43,37 @@ end
 
 def output
   @output ||= Output.new
+end
+
+# The shell automagically does this part
+# Leave out some stuff like escaping quotes
+# Just don't nest string or quotes in tests!
+def parse_options(opt_string)
+  options = []
+  word = ""
+  in_string = false
+  string_delimiter = ""
+
+  opt_string.each_char do |c|
+    if !in_string && (c == '"' || c == '\'')
+      in_string = true
+      string_delimiter = c
+    elsif in_string && c == string_delimiter
+      in_string = false
+    elsif c =~ /\s/ && !in_string
+      options << word unless word.chomp == ''
+      word = ""
+    else
+      word << c
+    end
+  end
+
+  # Add the last argument
+  if in_string
+    raise "Unterminated string!"
+  else
+    options << word
+  end
+
+  options
 end
